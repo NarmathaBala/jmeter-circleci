bdd0d0d44889da8786a04dee968c6405af28cac7
Neyissa Exilus <nexilus@microsoft.com>
2021-11-16 13:18:08 -0500

    Testing changes for the deploy script

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -56,7 +56,6 @@
 
     # Login to Azure via Service Principal
     echo "#### Attempting az login via service principal ####"
-    $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
     az login \
         --service-principal \
         --username="$service_principal_id" \
